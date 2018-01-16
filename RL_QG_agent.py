531fd358ca6652d63daf7e7b5bd6f7a74a6c78ad
Gosicfly <512999201@qq.com>
2018-01-16 12:15:28 +0800

    更新代码，模型

--- RL_QG_agent.py
+++ RL_QG_agent.py
@@ -6,24 +6,27 @@ import torch.optim as optim
 from torch.distributions import Categorical
 import os
 
-gamma = 0.9
+gamma = 0.99
 
 class PolicyNet(nn.Module):
     def __init__(self):
         super(PolicyNet, self).__init__()
-        self.affine1 = nn.Linear(3 * 64, 2 * 64)
-        self.affine2 = nn.Linear(2 * 64, 64)
+        self.affine1 = nn.Linear(2 * 64, 3 * 64)
+        self.affine2 = nn.Linear(3 * 64, 2 * 64)
+        self.affine3 = nn.Linear(2 * 64, 64)
         self.saved_log_probs = []
         self.step = 0
 
-    def forward(self, observation, enables, train):
+    def forward(self, observation, enables, player, train):
         if enables[0] == 65:
             return 65
         self.step += 1
-        observation = observation.reshape(3*64)
+        observation = observation[[0, 2], :] if player == 0 else observation[[1, 2], :]
+        observation = observation.reshape(2*64)
         observation = Variable(torch.from_numpy(observation), volatile=(not train)).float()
         x = F.relu(self.affine1(observation))
-        x = self.affine2(x)
+        x = F.tanh(self.affine2(x))
+        x = self.affine3(x)
         x = x[enables]
         probs = F.softmax(x, dim=-1)
         sampler = Categorical(probs)
@@ -34,18 +37,19 @@ class PolicyNet(nn.Module):
 class RL_QG_agent:
     def __init__(self, train=True):
         self.train = train
+        self.player = 0
         self.model_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "model")
-        self.model = PolicyNet()
         if train:
             self.model = PolicyNet()
-            self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+            self.optimizer = optim.Adam(self.model.parameters(), lr=0.0001)
         else:
             self.load_model()
 
-    def place(self,state,enables):
+    def place(self,state, enables, player):
         # 这个函数 主要用于测试， 返回的 action是 0-63 之间的一个数值，
         # action 表示的是 要下的位置。
-        action = self.model(state, enables, self.train)
+        self.player = player
+        action = self.model(state, enables, player, self.train)
         return action
 
     def finish_episode(self, r, train):
@@ -58,6 +62,8 @@ class RL_QG_agent:
             R = r + gamma * R
             rewards.insert(0, R)
         rewards = torch.Tensor(rewards)
+        if self.player == 1:
+            rewards = -rewards
         for log_prob, reward in zip(self.model.saved_log_probs, rewards):
             policy_loss.append(-log_prob * reward)
         self.optimizer.zero_grad()
--- reversi_main.py
+++ reversi_main.py
@@ -7,49 +7,59 @@ from RL_QG_agent import RL_QG_agent
 env = gym.make('Reversi8x8-v0')
 env.reset()
 
-agent = RL_QG_agent(train=False)
+agent = RL_QG_agent(train=True)
 
-max_epochs = 100
+max_epochs = 100000
 model_win = 0
+k = 0
+try:
+    for i_episode in range(max_epochs):
+        observation = env.reset()
+        # observation  是 3 x 8 x 8 的 list,表示当前的棋局，具体定义在 reversi.py 中的 state
+        for t in range(100):
+            action = [1,2]
+            # action  包含 两个整型数字，action[0]表示下棋的位置，action[1] 表示下棋的颜色（黑棋0或者白棋1）
+            ################### 黑棋 ############################### 0表示黑棋
+            #  这部分 黑棋 是模型下棋
+            # env.render()  #  打印当前棋局
+            enables = list(set(env.possible_actions))
+            if len(enables) == 0:
+                action_ = env.board_size**2 + 1
+            else:
+                action_ = agent.place(observation, enables, 0)  # 调用自己训练的模型
+            action[0] = action_
+            action[1] = 0   # 黑棋 为 0
+            observation, reward, done, info = env.step(action)
+            ################### 白棋 ############################### 1表示白棋
+            # env.render()
+            enables = list(set(env.possible_actions))
+            if len(enables) == 0:
+                action_ = env.board_size ** 2 + 1 # pass
+            else:
+                action_ = random.choice(enables)
+            action[0] = action_
+            action[1] = 1  # 白棋 为 1
+            observation, reward, done, info = env.step(action)
+            if done:
+                agent.finish_episode(reward, agent.train)
+                black_score = len(np.where(env.state[0,:,:]==1)[0])
+                if black_score >32:
+                    model_win += 1
+                    k += 1
+                break
+        if (i_episode + 1) % 100 == 0:
+            print(k)
+            k = 0
+            print('Episode {} done'.format(i_episode + 1))
 
-for i_episode in range(max_epochs):
-    if (i_episode + 1) % 100 == 0:
-        print('Episode {} done'.format(i_episode + 1))
-    observation = env.reset()
-    # observation  是 3 x 8 x 8 的 list,表示当前的棋局，具体定义在 reversi.py 中的 state
-    for t in range(100):
-        action = [1,2]
-        # action  包含 两个整型数字，action[0]表示下棋的位置，action[1] 表示下棋的颜色（黑棋0或者白棋1）
-        ################### 黑棋 ############################### 0表示黑棋
-        #  这部分 黑棋 是模型下棋
-        # env.render()  #  打印当前棋局
-        enables = list(set(env.possible_actions))
-        if len(enables) == 0:
-            action_ = env.board_size**2 + 1
-        else:
-            action_ = agent.place(observation, enables)  # 调用自己训练的模型
-        action[0] = action_
-        action[1] = 0   # 黑棋 为 0
-        observation, reward, done, info = env.step(action)
-        ################### 白棋 ############################### 1表示白棋
-        # env.render()
-        enables = list(set(env.possible_actions))
-        if len(enables) == 0:
-            action_ = env.board_size ** 2 + 1 # pass
-        else:
-            action_ = random.choice(enables)
-        action[0] = action_
-        action[1] = 1  # 白棋 为 1
-        observation, reward, done, info = env.step(action)
-        if done:
-            agent.finish_episode(reward, agent.train)
-            black_score = len(np.where(env.state[0,:,:]==1)[0])
-            if black_score >32:
-                model_win += 1
-            break
+    if agent.train:
+        agent.save_model()
 
-if agent.train:
-    agent.save_model()
-
-print('模型胜利次数：{}\t总次数：{}'.format(model_win, max_epochs))
-print('模型胜率：{}'.format(model_win/max_epochs))
+    print('模型胜利次数：{}\t总次数：{}'.format(model_win, max_epochs))
+    print('模型胜率：{}'.format(model_win/max_epochs))
+except KeyboardInterrupt as e:
+    print(e)
+    if agent.train:
+        agent.save_model()
+    print('模型胜利次数：{}\t总次数：{}'.format(model_win, max_epochs))
+    print('模型胜率：{}'.format(model_win/max_epochs))
